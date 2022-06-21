d18dc17e0eeb639f8bb12e94dd028a0b83ffe3e6
shiqiqi <2418687931@qq.com>
2022-06-21 11:44:29 +0800

    引入vant组件库

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles/index.less'
+// vant组件库
+import Vant from 'vant'
+// vant全局样式
+import 'vant/lib/index.css'
+
+// 注册全局
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
